244128e031412793802d6226697ed2695583b317
h1s0s <hs94122@naver.com>
2023-05-16 11:38:02 +0900

    Initial commit

---
+++ src/test/kotlin/com/example/kopringBlog/KopringBlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kopringBlog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringBlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
